b51391d49f38be0bf207c658d15112c04d417621
rajaa153 <rajaa153@gmail.com>
2022-01-14 13:26:51 +0530

    776666667

--- src/com/one/apptest.java
+++ src/com/one/apptest.java
@@ -7,7 +7,7 @@ public class apptest {
 		  public static void main(String[] args) {
 		    
 		    System.out.println("Enter two numbers");
-		    int first = 10;
+		    int first uu= 10;
 		    int second = 20;
 		    
 		    System.out.println( first + " " + second);
